553f60573bd42f734035fd5e8112f854e3e94ddc
ccossec <camille.cossec@maanmittauslaitos.fi>
2023-02-27 23:14:43 +0200

    Last version of test extractor and test MD
    Update of the last versions of the extractor for xpath and the
    test metadata used for tests.

--- src/test/test_extractor_xpath.py
+++ src/test/test_extractor_xpath.py
@@ -1,25 +1,80 @@
-"""Unit tests for xpath extractor"""
+# Clear the console
+import os
+clear = lambda: os.system('cls')
+clear()
 
-from ..extract import extract_rule
-from ..loader import load_dataset_metadata
+
+"""Unit tests for xpath extractor"""
+import sys
+sys.path.append('C:/Users/CCOSSEC/Work Folders/Evaluator configuration/geoe3-quality-dashboard/geoe3-quality-dashboard/src/')
+from extract import extract_rule
+
+import xml.etree.ElementTree as ET
+from loader import load_dataset_metadata
 
 def test__extract_publication_date__text():
     """Test that this xpath extractor with value function ***text*** works"""
     test_model = {
-        'dataset-metadata': load_dataset_metadata('example-metadata/Dataset MD Bui-ES-5.xml')
+        'dataset-metadata': load_dataset_metadata('test_MD_Bui_EX_1.xml')
     }
 
     test_rule = {
         'extractionRule': {
             'source': "dataset-metadata",
             'type': "xpath",
-            'rule': "gmd:identificationInfo[1]/*/gmd:citation/*/gmd:date[./*/gmd:dateType/*/text()='publication']/*/gmd:date",
+            'rule': "//gmd:PositionalAccuracy//gmd:AbsolutePositionalAccuracy//gmd:MeanValuePositionalUncertainties",
             'value': "text"
         }
     }
 
     result = extract_rule(test_rule['extractionRule'], test_model)
-    print(f"value extracted via extraction rule: {result}")
+    print(f"Value extracted via extraction rule: {result}")
+    # assert result[0] == 'continual'
 
-    assert result == '2015-12-15'
+    return result
 
+
+"""Unit tests for API extractor"""
+
+from loader import load_API
+
+def test__extract_data_api_services():
+    # Create URL with requested service Id
+    # list of service Id :   39859,164572,157386,88383,157353
+
+    test_model = {
+        'dataset-API': load_dataset_metadata('test_MD_Bui_EX_1.xml')
+    }
+
+    test_rule = {
+        'extractionRule': {
+            'source': "API",
+            'type': "dot-notation",
+            'serviceId': "39859",
+            'url_start': "https://beta.spatineo-devops.com/api/public/geoe3-dashboard?privateAccessKey=RoYjl8a3NbJ3tx1Hh0-IK6iP55i3YnAhSpcWcD2xgwSE8jBBP0EA9Q&serviceIds=",
+            'rule': "['services'][0]['serviceInfo']['type']",
+            'value': "text"
+        }
+    }
+
+    result = extract_rule(test_rule['extractionRule'], test_model)
+
+    url_join = [url_start, serviceId]
+    url = "".join(url_join)
+
+    # Get data from API
+    api_data = load_API(url)
+
+    # Gets requested field from API data
+    field_value = api_data['services'][0]['serviceInfo']['type']
+    
+    print(f"Value extracted via extraction rule: {field_value}")
+
+    return field_value
+
+
+#result = test__extract_publication_date__text()
+#print(result)
+
+field_value = test__extract_data_api_services()
+print(field_value)
