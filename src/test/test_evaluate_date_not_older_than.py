173818e112bdb29a528a7b2f64241d9abdacf509
Tino Fotiou <tino.fotiou@gmail.com>
2023-04-05 09:22:16 +0000

    chore(evaluation): Add missing imports and update test cases

--- src/test/test_evaluate_date_not_older_than.py
+++ src/test/test_evaluate_date_not_older_than.py
@@ -1,8 +1,10 @@
 """Unit tests for date-not-older-than evaluators"""
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
+from src.evaluate import evaluate, evaluator_by_type
 
-from ..evaluate import evaluate_results
 
 def test__date_not_older_than__positive():
     """2015-12-15 should be within 6 months of 2016-01-01"""
@@ -18,8 +20,9 @@ def test__date_not_older_than__positive():
     }
 
     result = '2015-12-15'
-    
-    (value, explanation) = evaluate_results(test_rule['evaluationCriteria'], result, test_context)
+    max_value = 1
+
+    (value, explanation) = evaluate(test_rule['evaluationCriteria'], result, test_context, max_value, evaluator_by_type=evaluator_by_type)
 
     assert value == 1
     assert explanation == 'date 2015-12-15 not older than P6M'
@@ -39,8 +42,9 @@ def test__date_not_older_than__negative():
     }
 
     result = '2015-12-15'
-    
-    (value, explanation) = evaluate_results(test_rule['evaluationCriteria'], result, test_context)
+    max_value = 1
+
+    (value, explanation) = evaluate(test_rule['evaluationCriteria'], result, test_context, max_value, evaluator_by_type=evaluator_by_type)
 
     assert value == 0
     assert explanation == "date 2015-12-15 older than P6M"
